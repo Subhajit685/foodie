30691ebe00abce28017af92f4654fe2c656b899b
Subhajit685 <subjahit09111999@gmail.com>
2024-09-18 10:32:35 +0530

    set expir

--- backend/utils/genToken.js
+++ backend/utils/genToken.js
@@ -7,7 +7,7 @@ export const getToket = (id, res) =>{
     const token = jwt.sign({id}, process.env.SECRET_KEY, {expiresIn : "7d"})
 
     res.cookie("jwt_token", token, {
-        maxAge : Date.now() + 7 * 24 * 60 * 60 * 1000,
+        expires : new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
         httpOnly : true,
         secure: process.env.NODE_ENV !== "development",
         sameSite: "strict",
