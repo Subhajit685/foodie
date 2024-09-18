d7e115ddfc91953c8ca7770e07a43a0577a39b9e
Subhajit685 <subjahit09111999@gmail.com>
2024-09-18 11:32:05 +0530

    chenge

--- backend/utils/genToken.js
+++ backend/utils/genToken.js
@@ -1,16 +1,11 @@
 import jwt from "jsonwebtoken"
-import dotenv from "dotenv"
-
-dotenv.config()
 
 export const getToket = (id, res) =>{
-    const token = jwt.sign({id}, process.env.SECRET_KEY, {expiresIn : "7d"})
+    const token = jwt.sign({id}, process.env.SECRET_KEY)
 
     res.cookie("jwt_token", token, {
         expires : new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
         httpOnly : true,
-        secure: process.env.NODE_ENV !== "development",
-        sameSite: "strict",
     })
     return token
 }
