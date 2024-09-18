b3f05a935eaab71e2e26310a500587d0fb786f83
Subhajit685 <subjahit09111999@gmail.com>
2024-09-18 14:16:27 +0530

    chenge cookies

--- backend/utils/genToken.js
+++ backend/utils/genToken.js
@@ -4,8 +4,11 @@ export const getToket = (id, res) =>{
     const token = jwt.sign({id}, process.env.SECRET_KEY)
 
     res.cookie("jwt_token", token, {
-        expires : new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-        httpOnly : true,
-    })
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production", // Use secure flag only in production
+        sameSite: "None",  // Explicitly set SameSite to None for cross-site requests
+    });
+    
     return token
 }
