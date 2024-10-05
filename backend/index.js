efaff51b0ba70c7fe83e567af463d9405d6a86f5
Subhajit Mondal <subhajit09111999@gmail.com>
2024-10-05 10:56:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -30,16 +30,7 @@ app.use("/api/cart", cartRoute)
 app.use("/api/order", orderRoute)
 app.use("/images", express.static("uploads"))
 
-const __dirname = path.resolve()
 
-
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "/frontend/dist")))
-
-    app.get("*", (req, res)=>{
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-    })
-}
 
 
 app.listen(PORT, ()=>{
